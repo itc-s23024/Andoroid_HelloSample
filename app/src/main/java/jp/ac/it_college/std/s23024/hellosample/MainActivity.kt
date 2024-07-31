ff43e3e8ec40d5259e65eda15eb8601eae1c34ec
itc-s23024 <s23024@std.it-college.ac.jp>
2024-07-31 15:44:43 +0900

    chap1

---
+++ app/src/main/java/jp/ac/it_college/std/s23024/hellosample/MainActivity.kt
@@ -0,0 +1,40 @@
+package jp.ac.it_college.std.s23024.hellosample
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import jp.ac.it_college.std.s23024.hellosample.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        enableEdgeToEdge()
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.btClick.setOnClickListener {
+            val inputStr = binding.etName.text.toString()
+            binding.tvOutput.text = getString(R.string.tv_msg, inputStr)
+        }
+
+        binding.btClear.setOnClickListener {
+            binding.etName.text.clear()
+            binding.tvOutput.text = null
+        }
+    }
+}
